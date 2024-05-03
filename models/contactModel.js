1f4f5ffdee47bd6e8e9b562c56b9f96ca8bac4df
madhu502 <chaudharymadhu502@gmail.com>
2024-05-03 19:16:23 +0545

    assignment

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-    name :{type: String, required : true},
-    phone :{type: String, required : true},
-    email :{type: String, required : true},
-})
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
 
-const Contact = mongoose.model("contacts", contactSchema)
+const Contact = mongoose.model("contacts", contactSchema);
 
-module.exports=Contact
+module.exports = Contact;
