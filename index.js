fb5d9e9cdc5fe7140909ac651092a83da639e801
madhu502 <chaudharymadhu502@gmail.com>
2024-05-03 17:30:15 +0545

    assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userRoutes = require('./routes/userRoutes')
+
+require("dotenv").config()
+const connectDatabase =require('./database/database')
+
+const app = express();
+
+app.use(express.json())
+app.use("/api/users", userRoutes)
+
+const PORT = 5000;
+app.listen(5000, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
